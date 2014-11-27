2030f3f2f7d601d96c52c3921c067c8e74514d1f
Emin Tham <emintham@gmail.com>
2014-11-27 11:15:43 -0500

    WIP

--- func_util.py
+++ func_util.py
@@ -1,17 +1,27 @@
 import types
 
 
-def func_copy(f):
+def func_copy(f, name, bind=False):
     """
     Returns a new copy of the function passed in.
     """
-    new_f = types.FunctionType(
-                f.__code__,
-                f.__globals__,
-                name=f.__name__,
-                argdefs=f.__defaults__,
-                closure=f.__closure__
-            )
-    new_f.__dict__.update(f.__dict__)
+    if (not isinstance(f, types.FunctionType) and
+        not isinstance(f, types.MethodType)):
+        raise TypeError('Expecting a function type, got {}'.format(type(f)))
+    else:
+        if not name:
+            name = f.__name__
 
-    return new_f
+        new_f = types.FunctionType(
+                    f.__code__,
+                    f.__globals__,
+                    name=name,
+                    argdefs=f.__defaults__,
+                    closure=f.__closure__
+                )
+        new_f.__dict__.update(f.__dict__)
+
+        if bind:
+            return new_f
+        else:
+            bound_new_f = types.MethodType(new_f, 
--- test_decorator.py
+++ test_decorator.py
@@ -13,17 +13,17 @@ class FuncUtilTests(unittest.TestCase):
         return bar + 2
 
     def test_func_copy(self):
-        new_f = func_copy(foo)
+        new_f = func_copy(foo, 'new_foo')
         self.assertNotEqual(id(new_f), id(foo))
         self.assertEqual(foo(3), new_f(3))
 
     def test_unbound_method_copy(self):
-        new_f = func_copy(self.foo)
+        new_f = func_copy(self.foo, 'new_foo')
         self.assertNotEqual(id(new_f), id(self.foo))
         self.assertEqual(self.foo(1), new_f(object, 1))
 
     def test_bound_method_copy(self):
-        new_f = func_copy(self.foo)
+        new_f = func_copy(self.foo, 'new_foo')
         self.foo2 = types.MethodType(new_f, self)
         self.assertNotEqual(id(self.foo2), id(self.foo))
         self.assertEqual(self.foo(1), self.foo2(1))
